56d2a6af7d52a31c50731e009260867bfb6dee09
Michael Holm <michael@weblovers.se>
2023-10-01 00:03:47 +0200

    Step 2 completed

--- code/script.js
+++ code/script.js
@@ -20,7 +20,44 @@ const mikesPizzeria = () => {
   }
 
   // Step 2 - Food choice
-  // Your code goes here
+  //  To simplify the code and avoid repeating code we define a food object with the Food Types and arrays with the corresponding sub food types.
+  const food = {
+    "Pizza": ["Napolitana","Hawaian","Pepperoni"],
+    "Pasta": ["Spaghetti Carbonara","Fettuccine Alfredo","Cheesy Tortellini"],
+    "Salad": ["Caesar Salad","Caprese Salad","Greek Salad"]
+  }
+
+  // We prompt the customer for the number matching the food type and parse it to a integer.
+  const orderType = parseInt(
+    prompt (
+      `What type of food would you like to order?
+        Enter the number:
+        1. Pizza
+        2. Pasta
+        3. Salad
+      `
+    )
+  );
+
+  // We define a variable to store the selected food type as a sting
+  let foodType = "";
+
+  // If the customer selcted 1,2 or 3 we save the foodType and confirm their choice. If they entered anything else we prompt them with a error and shutdown.
+  if (orderType === 1) {
+    foodType = "Pizza";
+  }
+  else if (orderType === 2) {
+    foodType = "Pasta";
+  }
+  else if (orderType === 3) {
+    foodType = "Salad";
+  }
+  else {
+    alert(`You selected an invalid option. Please come back later.`);
+    return;
+  }
+
+  alert(`Great choice! You selected ${foodType}`);
 
   // Step 3 - Subtype choice
   // Your code goes here
